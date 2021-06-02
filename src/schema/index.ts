560f8acaa1763508f1da742e08b227bfd3e04e66
USER <user@AD01423500.local>
2021-06-02 21:29:12 +0900

    query 타입 요청에 익숙해지기

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,12 +3,32 @@ import { gql } from 'apollo-server-koa'
 export default gql`
     type Query {
         hello: String
+        totalPhotos(id: Int=1): Int!
+        allPhotos(sort: SortDirection = ASCENDING): [Photo!]!
+        totalUsers: Int!
+        allUsers: [User!]!
     }
     
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+    }
+
     type Photo {
         id: ID!
         name: String!
         url: String!
         description: String!
+    }
+
+    enum SortDirection {
+        ASCENDING
+        DESCENDING
+    }
+
+    schema {
+        query: Query
     }
 `
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,22 @@
 import Koa from 'koa'
-import { ApolloServer, gql } from 'apollo-server-koa'
+import {ApolloServer, gql, IResolvers} from 'apollo-server-koa'
 import typeDefs from '../schema'
 
 // Provide resolver functions for your schema fields
-const resolvers = {
+const resolvers: IResolvers = {
     Query: {
         hello: () => 'Hello world!',
+        totalPhotos:(source, args) => {
+           return args.id
+        },
+        allPhotos: (source, args) => {
+            const photos = [ { id: '1', name: 'nick1', url: 'www.example-1.com', description: 'description' }, { id: '2', name: 'nick2', url: 'www.example-2.com', description: 'description' } ]
+            if (args.sort === 'ASCENDING') {
+                return photos.sort((a, b) => Number(b.id) - Number(a.id))
+            }
+
+            return photos
+        }
     },
 };
 
